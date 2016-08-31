f960800bdb455de9928463b7f5b1df1f1b3a9894
Li <15311759611@163.com>
2016-08-31 16:01:16 +0800

    数据库

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'default' => array (
-'hostname' => 'my3382257.xincache1.cn','database' => 'my3382257','username' => 'my3382257','password' => 'Z2A3V9M7','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+'hostname' => '47.90.41.189','database' => 'paoma','username' => 'root','password' => 'paoma~!@','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
 );
