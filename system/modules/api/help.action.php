673d8eb787045745ab7a8fe757a7416af5a3dade
BLN <praise777@163.com>
2016-09-18 10:41:50 +0800

    帮助

---
+++ system/modules/api/help.action.php
@@ -0,0 +1,46 @@
+<?php
+
+class help extends SystemAction {
+
+//问题详情
+	public function issuelist(){
+		
+		$db = System::load_sys_class('model');
+		$id = isset($_POST['id']) ? $_POST['id'] : null;
+		$data =  $db->GetOne("select issue reply from `@#_help` where id=$id ");
+		if($data) {
+			$code = 200;
+			$msg = "查询成功";
+		}else {
+			$code = 100;
+			$msg = "数据为空";
+		}
+		$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+		echo json_encode($json);
+		
+
+	}
+//提问
+	public function issue(){
+		
+		$db = System::load_sys_class('model');
+		$id = isset($_POST['id']) ? $_POST['id'] : null;
+		$issue = isset($_POST['issue']) ? $_POST['issue'] : null;
+		
+		if($issue) {
+			$code = 200;
+			$msg = "添加成功";
+			$data =  $db->Query("INSERT INTO `@#_help` (`id`, `issue`) VALUES ('$id',  '$issue')");
+		}else {
+			$code = 100;
+			$msg = "数据为空";
+		}
+		$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+		echo json_encode($json);
+		
+
+	}
+
+}
+
+?>
