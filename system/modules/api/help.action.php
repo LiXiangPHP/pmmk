e064ea353608be755c14e9789072b947ecfbd7bf
BLN <praise777@163.com>
2016-09-18 10:58:49 +0800

    更改帮助

--- system/modules/api/help.action.php
+++ system/modules/api/help.action.php
@@ -24,20 +24,20 @@ class help extends SystemAction {
 	public function issue(){
 		
 		$db = System::load_sys_class('model');
-		$id = isset($_POST['id']) ? $_POST['id'] : null;
+		$uid = isset($_POST['uid']) ? $_POST['uid'] : null;
 		$issue = isset($_POST['issue']) ? $_POST['issue'] : null;
 		
 		if($issue) {
 			$code = 200;
 			$msg = "添加成功";
-			$data =  $db->Query("INSERT INTO `@#_help` (`id`, `issue`) VALUES ('$id',  '$issue')");
+			$data =  $db->Query("INSERT INTO `@#_help` (`uid`, `issue`) VALUES ('$uid',  '$issue')");
 		}else {
 			$code = 100;
 			$msg = "数据为空";
 		}
 		$json = array('code' => $code, 'msg' => $msg, 'data' => $data); 
 
-		
+
 		echo json_encode($json);
 		
 
