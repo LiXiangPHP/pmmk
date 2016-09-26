47e8ee12cbb82e6fc3ad5d135414da97d0ee8315
BLN <praise777@163.com>
2016-09-26 16:19:06 +0800

    jjj

---
+++ system/modules/api/snatch.action.php
@@ -0,0 +1,13 @@
+<?php
+
+class snatch extends SystemAction {
+	public function mm(){
+		$db = System::load_sys_class('model');
+		$type = isset($_POST['type']) ? $_POST['type'] : null;
+		if($type==1){
+
+		}
+	}
+}
+
+?>
