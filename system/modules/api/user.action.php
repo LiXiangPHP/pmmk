82a2975d5e45ab7a8606dd269d5d375d53eb1c22
Li <15311759611@163.com>
2016-09-27 18:14:17 +0800

    注册接口

--- system/modules/api/user.action.php
+++ system/modules/api/user.action.php
@@ -33,9 +33,43 @@ class user extends SystemAction {
 		}
 	//注册接口
 	//
-	public 	function register()
+	public function register()
 	{
-		
+
+		$db = System::load_sys_class('model');	
+		$name=$_POST['username'];
+		$password=$_POST['password'];
+		$verify=$_POST['verify'];
+		$code = $_COOKIE['code'];
+		if($verify != $code || !$verify)
+		{
+			$code = 100;
+			$msg = '验证码错误';
+			$json = array('code' => $code, 'msg' => $msg);
+			echo json_encode($json);die;
+		}
+		$member=$db->GetOne("SELECT * FROM `@#_member` WHERE `mobile` = '$name' LIMIT 1");
+		// print_r($member);die;
+		if(is_array($member) && $member['mobile'] == $name){
+
+				$code = 100;
+				$msg = '该手机号已被注册';
+				$json = array('code' => $code, 'msg' => $msg);
+				echo json_encode($json);die;
+			}
+
+		$time=time();
+		$userpassword=md5($password);
+		$sql="INSERT INTO `@#_member`(username,mobile,password,img,emailcode,mobilecode,time)VALUES('$name','$name','$userpassword','photo/member.jpg','-1','1','$time')";
+		$sqlreg = $db->Query($sql);
+		if($sqlreg)
+		{
+			$code = 200;
+			$msg = '注册成功';
+			$data  = md5($name.$password.$time);
+			$json = array('code' => $code, 'msg' => $msg ,'data'=>$data);
+			echo json_encode($json);die;
+		}
 	}	
 		
 }
