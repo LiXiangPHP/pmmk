8161b561b5c14f7f42ba9ae4ccf89af228eebf5c
BLN <praise777@163.com>
2016-09-28 14:33:16 +0800

    1515141

--- system/modules/api/paylog.action.php
+++ system/modules/api/paylog.action.php
@@ -2,27 +2,28 @@
 
 class paylog extends SystemAction {
 	public function lg(){
-		$db = System::load_sys_class('model');
 		$token = isset($_POST['token']) ? $_POST['token'] : null;
 		$info = System::token_uid($token);
-		if ($info['code']==200) {												
-			$money = $db->GetList("select money from `@#_member` where uid='$info[uid]' ");
-			$paylog = $db->GetList("select time,pay_type,money from `@#_member` where uid='$info[uid]'");
-			print_r($paylog);die();
-			if($data) {
+		if ($info['code']==200) {
+			$db = System::load_sys_class('model');
+			$money =  $db->GetOne("select money from `@#_member` where uid='$info[uid]' ");
+			$data = $data = $db->GetList("select time,pay_type,money from `@#_member_addmoney_record` where uid='$info[uid]'");
+			if($data&&$money) {
 				$code = 200;
 				$msg = "查询成功";
 			}else {
 				$code = 400;
 				$msg = "数据为空";
 			}
-			$json = array('type' => $type,'code' => $code, 'msg' => $msg, 'data' => $data);
-			echo json_encode($json);			
+			$json = array('code' => $code, 'msg' => $msg, 'moneytoal' => $money, 'data' => $data);
+			echo json_encode($json);
 		}else{
 			$json = array('code' => 300, 'msg' => '请登录', 'data' => $data);
 			echo json_encode($json);
-		}	
+		}
 		
+		
+
 	}
 }
 
