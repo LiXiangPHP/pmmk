e637085c6e9fb92a2d24e92f2ccc191f4bc933b9
susususucl <1270539027@qq.com>
2016-09-30 14:15:10 +0800

    no message

--- system/modules/api/my.action.php
+++ system/modules/api/my.action.php
@@ -131,7 +131,142 @@ class my extends SystemAction {
 	}
 
 	
+	//签到接口
+	public function json_signday() {
+		$code = '';
+		$msg  = '';
+		$data = array();
+		// $pagenum = abs(intval($_POST['p']));
+		$token = trim($_POST['token']);
+		$info = System::token_uid($token);
+		if($info['code'] == 100) {
+			$code = 300;
+			$msg = "用户未登录";
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);die;
+		}
 
+	}
+
+	//签到
+	public function json_sign() {
+		$code = '';
+		$msg  = '';
+		$data = array();
+		$token = trim($_POST['token']);
+		$info = System::token_uid($token);
+		if($info['code'] == 100) {
+			$code = 300;
+			$msg = "用户未登录";
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);die;
+		}
+		# 签到时间限制（不能夸天哦。。）
+		$time_start = '00:01';
+		$time_stop= '23:59';
+		
+		# 每日签到增加福分
+		$score = $this->db->GetOne("select points from `@#_signrules` where number = 1 limit 1");
+		if($score) {
+			$time_score = $score['points'];
+		}else {
+			$time_score = 0;
+		}
+
+		$member = $this->db->GetOne("select * from `@#_member` where uid = '$info[uid]' limit 1");
+		$days = $this->db->GetList("select * from `@#_signrules` order by number asc");
+		
+		# 连续签到最大的天数
+		$max_day = 30;
+		// $num = count($days);
+		// $max_day = $days[$num-1]['number']; 
+		
+		# 连续签到增加的福分（在后面查询替换）
+		$time_day_score = 10;
+
+		if ( !$member['mobile'] || $member['mobilecode']!='1' ) {
+			$code = 100;
+			$msg = "用户手机未验证不可签到";
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);die;
+		}
+
+		if ( $member['sign_in_date'] == date('Y-m-d') ) {
+			$code = 100;
+			$msg = "已签到";
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);die;
+		}
+		if ( strtotime(date('Y-m-d').$time_start ) > time() || strtotime(date('Y-m-d').$time_stop ) < time() ) {
+			$code = 100;
+			$msg = "超出签到时间";
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);die;
+		}
+
+		if ( $member['sign_in_date'] == date('Y-m-d',strtotime('-1 day')) ){# 连续签到
+			
+			if ( $member['sign_in_time'] >= $max_day ) {//签到天数
+				$member['sign_in_time'] = 0;
+			}
+
+			$sign_in_time = $member['sign_in_time'] + 1;
+			$sign_in_time_all = $member['sign_in_time_all'] + 1;
+			$sign_in_date = date('Y-m-d');
+			$score = $member['score'] + $time_score;
+			
+
+			if ( $sign_in_time >= $max_day ) {
+				# 领取大礼包了
+				$score += $time_day_score;
+				$big = true;
+			} else {
+				$big = false;
+			}
+
+			// for($k = 1;$k <= $num-2;$k++) {
+			// 	if ( $sign_in_time >= $days[$k]['number'] && $sign_in_time < $days[$k+1]['number']) {# 领取大礼包了
+			// 		$score += $days[$k-1]['points'];
+			// 		$big = true;
+			// 	} else if ($k == $num-1 && $sign_in_time == $max_day) {
+			// 		$score += $days[$k]['points'];
+			// 		$big = true;
+			// 	}else {
+			// 		$big = false;
+			// 	}
+			// }
+
+			//积分明细记录
+			// $this->db->Query("INSERT INTO `@#_member_account` (`uid`, `type`, `pay`, `content`, `money`, `time`) VALUES ('".$member['uid']."', '1', '福分', '每日签到', '$time_score', '".time()."')");
+			$this->db->Query("UPDATE `@#_member` SET score='".$score."',sign_in_time='".$sign_in_time."', sign_in_time_all='".$sign_in_time_all."', sign_in_date='".$sign_in_date."' where uid='".$member['uid']."'");
+			if ( $big ) {
+				$this->db->Query("INSERT INTO `@#_member_account` (`uid`, `type`, `pay`, `content`, `money`, `time`) VALUES ('".$member['uid']."', '1', '福分', '签到大礼包', '$time_day_score', '".time()."')");
+				//提示信息
+				$code = 200;
+			} else {
+				//提示信息
+				$code = 100;
+			}
+
+		} else {//签到不连续
+			$sign_in_time = 1;
+			$sign_in_time_all = $member['sign_in_time_all'] + 1;
+			$sign_in_date = date('Y-m-d');
+			$score = $member['score'] + $time_score;
+			$this->db->Query("INSERT INTO `@#_member_account` (`uid`, `type`, `pay`, `content`, `money`, `time`) VALUES ('".$member['uid']."', '1', '福分', '每日签到', '$time_score', '".time()."')");
+			$this->db->Query("UPDATE `@#_member` SET score='".$score."',sign_in_time='".$sign_in_time."', sign_in_time_all='".$sign_in_time_all."', sign_in_date='".$sign_in_date."' where uid='".$member['uid']."'");
+			//提示信息
+		}
+
+		//签到列表
+		// if ( !$member['sign_in_date'] ) {
+		// 	$member['sign_in_date'] = '-';
+
+		// }else if ( $member['sign_in_date'] != date('Y-m-d') && $member['sign_in_date'] != date('Y-m-d',strtotime('-1 day')) ) {
+
+		// 	$member['sign_in_time'] = 0;
+		// }
+	}
 
 
 	//每日签到
@@ -149,6 +284,7 @@ class my extends SystemAction {
 			echo json_encode($json);die;
 		}
 	}
+	
 	//积分兑换展示
 	public function dhzs() {
 		$db = System::load_sys_class('model');
