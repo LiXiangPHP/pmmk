85938a7dfc2eac176342fdcbdf925e7f4e3e7c7a
zyf <783339950@qq.com>
2016-10-09 16:05:33 +0800

    111

--- system/modules/api/cart.action.php
+++ system/modules/api/cart.action.php
@@ -13,7 +13,14 @@ class cart extends SystemAction {
         $info = System::token_uid($uid);
         if ($info['code']==200) {
             $db = System::load_sys_class('model');
-            if (!empty($id)&!empty($num)&!empty($info['uid'])&($db->Query("INSERT INTO `@#_shopcart` (`user_id`, `good_id`,`num`) VALUES ('$info[uid]','$id','$num')")!=false)) {
+            $uidm = $db->GetOne("SELECT * FROM `@#_shopcart` WHERE good_id = '$id' AND user_id = '$info[uid]'");
+            if(!empty($uidm)){
+                $numm =$num + $uidm['num'];
+                $numadd = $db->Query("UPDATE `@#_shopcart` SET num='$numm' WHERE good_id = '$id' AND user_id = '$info[uid]'");
+                $code = 200;
+                $msg = "添加成功";
+            }
+            elseif (!empty($id)&!empty($num)&!empty($info['uid'])&($db->Query("INSERT INTO `@#_shopcart` (`user_id`, `good_id`,`num`) VALUES ('$info[uid]','$id','$num')")!=false)) {
                 $code = 200;
                 $msg = "添加成功";
             } else {
