e3311bee563700a9a16104c8c062a05858ad9777
lhj <905383508@qq.com>
2016-10-12 12:59:10 +0800

    收货确认

--- system/modules/api/order.action.php
+++ system/modules/api/order.action.php
@@ -1,6 +1,7 @@
 <?php
 
 class order extends SystemAction {
+	//中奖收货
 	public function od(){
 		$token = isset($_POST['token']) ? $_POST['token'] : null;
 		$info = System::token_uid($token);
@@ -26,7 +27,7 @@ class order extends SystemAction {
 			//已发货，待收货
 			if ($type==2) {
 				$db = System::load_sys_class('model');
-				$data = $db->GetList("select b.q_end_time,a.shopname,a.shopqishu,b.thumb from `@#_member_go_record` as a,`@#_shoplist` as b where a.shopid=b.id and  a.uid='$info[uid]' and a.huode>10000000 and status like '%已发货,待收货%' ");
+				$data = $db->GetList("select b.q_end_time,a.shopname,a.shopqishu,a.id,b.thumb from `@#_member_go_record` as a,`@#_shoplist` as b where a.shopid=b.id and  a.uid='$info[uid]' and a.huode>10000000 and status like '%已发货,待收货%' ");
 				foreach($data as $k=>$v) {
 				$data[$k]['thumb'] = "gangmaduobao.com/statics/uploads/".$v['thumb'];
 				}
@@ -43,7 +44,7 @@ class order extends SystemAction {
 			//已收货
 			if ($type==3) {
 				$db = System::load_sys_class('model');
-				$data = $db->GetList("select b.q_end_time,a.shopname,a.shopqishu,b.thumb from `@#_member_go_record` as a,`@#_shoplist` as b where a.shopid=b.id and  a.uid='$info[uid]' and a.huode>10000000 and status like '%已发货,已完成%' ");
+				$data = $db->GetList("select b.q_end_time,a.shopname,a.shopqishu,a.id,b.thumb from `@#_member_go_record` as a,`@#_shoplist` as b where a.shopid=b.id and  a.uid='$info[uid]' and a.huode>10000000 and status like '%已发货,已完成%' ");
 				foreach($data as $k=>$v) {
 				$data[$k]['thumb'] = "gangmaduobao.com/statics/uploads/".$v['thumb'];
 				}
@@ -65,6 +66,25 @@ class order extends SystemAction {
 		
 		
 
+	}
+	//确认收货接口
+	public function qr(){
+		
+		$info = System::token_uid($token);
+		$id = isset($_POST['id']) ? $_POST['id'] : null;
+		$token = isset($_POST['token']) ? $_POST['token'] : null;
+		$data = $db->GetList("select * from `@#_member_go_record` where id=$id  ");
+		if($data) {
+					$code = 200;
+					$msg = "成功";
+					$data = $db->Query("update `@#_member_go_record` set status='已付款,已发货,已完成' where id=$id ") ;
+				}else {
+					$code = 400;
+					$msg = "失败";
+				}
+				$json = array('code' => $code, 'msg' => $msg );
+				echo json_encode($json);
+	
 	}
 }
 
