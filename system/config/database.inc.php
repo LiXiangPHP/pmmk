69c2a2c36da5d358097b0c93a8e4f4604105bacc
Li <15311759611@163.com>
2016-10-14 17:41:15 +0800

    fix

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'default' => array (
-'hostname' => '47.90.41.189','database' => 'paoma','username' => 'root','password' => 'paoma~!@','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+'hostname' => '47.90.41.189','database' => 'pm','username' => 'root','password' => 'paoma~!@','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
 );
