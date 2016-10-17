07ac45e5a1db515a49efc09d49ea7f39c27ea8c1
zyf <783339950@qq.com>
2016-10-17 17:22:50 +0800

    111

--- system/modules/api/cart.action.php
+++ system/modules/api/cart.action.php
@@ -40,7 +40,13 @@ class cart extends SystemAction {
         $info = System::token_uid($uid);
         if ($info['code']==200) {
                 $db = System::load_sys_class('model');
-                $data = $db->GetList("SELECT l.id,l.title,l.money,l.yunjiage,l.canyurenshu,l.shenyurenshu,l.thumb,c.num FROM `@#_shoplist` l,`@#_shopcart` c,`@#_member` m WHERE m.uid = '$info[uid]' AND l.id = c.good_id AND c.user_id =  m.uid");
+                $Sdata = $db->GetList("SELECT l.id,l.title,l.money,l.yunjiage,l.canyurenshu,l.shenyurenshu,l.thumb,c.num FROM `@#_shoplist` l,`@#_shopcart` c,`@#_member` m WHERE m.uid = '$info[uid]' AND l.id = c.good_id AND c.user_id =  m.uid");
+            foreach($Sdata as $v) {
+                if($v['thumb']) {
+                    $v['thumb'] = "gangmaduobao.com/statics/uploads/".$v['thumb'];
+                }
+                $data['data'][] = $v;
+            }
             if (!empty($data)) {
                 $code = 200;
                 $msg = "查询成功";
@@ -74,100 +80,6 @@ class cart extends SystemAction {
             $json = array('code' => $code, 'msg' => $msg);
             echo json_encode($json);
         }else {
-            $json = array('code' => 300, 'msg' => '请登录', 'data' => $data);
-            echo json_encode($json);
-        }
-    }
-
-
-    /*购物车购买接口*/
-    public function json_cartbuy(){
-        $data = $_POST['data'];
-        $type = $_POST['type'];
-        $uid = $_POST['uid'];
-        $info = System::token_uid($uid);
-        if ($info['code']==200) {
-            $Sdata = json_decode(stripslashes($data), true);
-//            print_r($Sdata);
-            $shopids = '';
-            if (is_array($Sdata)) {
-                foreach ($Sdata as $k => $v) {
-                    foreach ($v as $i => $j) {
-                        $newdata [] = $j;
-                    }
-                }
-            }
-//            print_r($newdata);
-            $MoenyCount ='';
-            foreach ($newdata as $v) {
-                $MoenyCount = $MoenyCount + $v['personcount'];
-            }
-//            print_r($MoenyCount);
-            $cartdata['moenycount'] = $MoenyCount;
-//            print_r($cartdata);
-            foreach ($newdata as $v) {
-                $shopids = $shopids . ',' . $v['id'];
-                $shopids = trim($shopids, ',');
-            }
-            $time = time();
-            $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
-            $dingdancode = $yCode[intval(date('Y')) - 2011] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%02d', rand(0, 99));
-//            print_r($dingdancode);
-            if(iconv_strlen($shopids)>1){
-               $dingdancode_tmp = 1;	//多个商品相同订单
-            }else{
-               $dingdancode_tmp = 0;	//单独商品订单
-            }
-            echo "\n";
-//            print_r($dingdancode_tmp);
-            echo "\n";
-//            print_r($shopids);
-//            print_r($info['uid']);
-            $db = System::load_sys_class('model');
-            $member = $db->GetOne("SELECT * FROM `@#_member` WHERE `uid` = '$info[uid]'");
-//            print_r($member);
-            $username = $member['username'];
-            $uphoto = $member['img'];
-            $ip = $member['user_ip'];
-            $shoplist = $db->GetList("SELECT * FROM `@#_shoplist` WHERE `id` IN ('$shopids')");
-//            print_r($shoplist);
-            foreach ($shoplist as $v) {
-                $shoptitle = $shoptitle . ',' . $v['title'];
-                $shoptitle = trim($shoptitle, ',');
-            }
-//            print_r($shoptitle);
-            foreach ($newdata as $v) {
-                $shopqishu = $shopqishu . ',' . $v['date'];
-                $shopqishu = trim($shopqishu, ',');
-            }
-//            print_r($shopqishu);
-            if($type = 1){
-                $pay_type = 'zhanghu';
-            }elseif($type = 2){
-                $pay_type = 'weixin';
-            }elseif($type = 3){
-                $pay_type = 'zhifubao';
-            }else{
-                echo '支付方式不正确';
-            }
-//            echo $pay_type;
-            $status='未付款,未发货,未完成';
-            $cartdata['pay_type'] = $pay_type;
-            $db = System::load_sys_class('model');
-            $order = $db->Query("INSERT INTO `@#_member_go_record` (`code`,`code_tmp`,`username`,`uphoto`,`uid`,`shopid`,`shopname`,`shopqishu`,`gonumber`,`goucode`,`moneycount`,`pay_type`,`ip`,`status`,`time`) VALUES ('$dingdancode','$dingdancode_tmp','$username','$uphoto','$info[uid]','$shopids','$shoptitle','$shopqishu','$MoenyCount','$dingdancode','$MoenyCount','$pay_type','$ip','$status','$time')");
-            $data = $db->GetOne("SELECT `moneycount`,`pay_type`,`code` FROM `@#_member_go_record` WHERE `uid` = '$info[uid]' AND `time`= '$time'");
-            if(!empty($shopids)&!empty($info['uid'])&!empty($data)){
-                $code = 200;
-                $msg = "订单提交成功";
-//                print_r($data);
-            } else {
-                $code = 100;
-                $msg = "订单提交失败";
-            }
-                $json = array('code' => $code, 'msg' => $msg,'data'=> $data);
-                echo json_encode($json);
-
-        }else{
             $json = array('code' => 300, 'msg' => '请登录', 'data' => $data);
             echo json_encode($json);
         }
