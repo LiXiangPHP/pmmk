d96581fc90f903221615198f93dc492b48774cad
lhj <905383508@qq.com>
2016-10-17 09:32:37 +0800

    no message

--- system/modules/api/shop.action.php
+++ system/modules/api/shop.action.php
@@ -425,7 +425,31 @@ class shop extends SystemAction {
 			echo json_encode($json);
 		}
 	}
-
+	/*猜你喜欢*/
+	public function love() {
+		$token = isset($_POST['token']) ? $_POST['token'] : null;
+		$info = System::token_uid($token);
+		if ($info['code']==200) {
+			$db = System::load_sys_class('model');
+			$data = $db->GetList("select id,thumb,title,money,qishu,canyurenshu,shenyurenshu from `@#_shoplist` where `q_uid` is null and `renqi` = '1' order by id DESC LIMIT 6 ");
+			foreach($data as $k=>$v) {
+				$data[$k]['thumb'] = "gangmaduobao.com/statics/uploads/".$v['thumb'];
+			}
+		if($data) {
+				$code = 200;
+				$msg = "查询成功";
+			}else {
+				$code = 400;
+				$msg = "数据为空";
+			}
+			$json = array('code' => $code, 'msg' => $msg, 'data' => $data);
+			echo json_encode($json);
+		}else{
+			$json = array('code' => 300, 'msg' => '请登录', 'data' => $data);
+			echo json_encode($json);
+		}
+		
+	}
 
 }
 
