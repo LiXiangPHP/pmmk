44ddec5d82225b3f16d7fb570d8a5c6e23b373d9
ZYF <783339950@qq.com>
2016-10-31 16:46:07 +0800

    1111

--- system/modules/api/cart.action.php
+++ system/modules/api/cart.action.php
@@ -13,34 +13,31 @@ class cart extends SystemAction {
             $db = System::load_sys_class('model');
             $uidm = $db->GetOne("SELECT * FROM `@#_shopcart` WHERE good_id = '$id' AND user_id = '$info[uid]'");
             $shenyu = $db->GetOne("SELECT shenyurenshu FROM `@#_shoplist` WHERE id = '$id'");
+
+            $cartm =$uidm['num'];
+//            print_r($cartm);die;
 //            print_r($shenyu);die;
 //            print_r($uidm);die;
-            if($num>$shenyu['shenyurenshu']){
-                $code = 100;
-                $msg = "购买人数大于剩余人数";
-                $json = array('code' => $code, 'msg' => $msg);
-                echo json_encode($json);die;
-            }
-            if(!empty($uidm)){
-                $numm =$num + $uidm['num'];
-                $numadd = $db->Query("UPDATE `@#_shopcart` SET num ='$numm' WHERE good_id = '$id' AND user_id = '$info[uid]'");
-                if(!empty($numadd)){
-                $code = 200;
-                $msg = "添加成功";
-                }
-            }elseif (!empty($id)&!empty($num)&!empty($info['uid'])&($db->Query("INSERT INTO `@#_shopcart` (`user_id`, `good_id`,`num`) VALUES ('$info[uid]','$id','$num')")!=false)) {
-                $code = 200;
-                $msg = "添加成功";
-            } else {
+            $cartnum = $cartm + $num;
+            if($cartnum>$shenyu['shenyurenshu']) {
                 $code = 100;
                 $msg = "添加失败";
+                $json = array('code' => $code, 'msg' => $msg);
+                echo json_encode($json);die;
+            }else{
+                $numadd = $db->Query("UPDATE `@#_shopcart` SET num ='$cartnum' WHERE good_id = '$id' AND user_id = '$info[uid]'");
+                if (!empty($numadd)) {
+                    $code = 200;
+                    $msg = "添加成功";
+                    $json = array('code' => $code, 'msg' => $msg);
+                    echo json_encode($json);
+                }
             }
-            $json = array('code' => $code, 'msg' => $msg);
-            echo json_encode($json);
-        }else {
+            }else {
             $json = array('code' => 300, 'msg' => '请登录', 'data' => $data);
             echo json_encode($json);
         }
+
     }
 
     /*查看购物车接口*/
